6e733576512cba0aa1fffd133f0851155a97e46b
abel <abeldzan@gmail.com>
2020-06-23 01:32:06 +0000

    the `Disease` Model

---
+++ lib/Screens/disease_search.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DiseaseSearch extends StatefulWidget {
+  @override
+  _DiseaseSearchState createState() => _DiseaseSearchState();
+}
+
+class _DiseaseSearchState extends State<DiseaseSearch> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
